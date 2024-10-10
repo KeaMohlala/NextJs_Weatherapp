9ae04a7f603429e9948e2859ea7202c3f5187544
KeaMohlala <137070041+KeaMohlala@users.noreply.github.com>
2024-10-10 21:17:33 +0200

    simplify home page only import app.tsx

--- my-weather-app/src/pages/index.tsx
+++ my-weather-app/src/pages/index.tsx
@@ -1,85 +1,10 @@
 import React, { useState } from "react";
-import { GetServerSideProps } from "next";
-import WeatherSearch from "../components/src/weathersearch";
+import App from "../components/src/App";
 
-interface HomeProps {
-  initialData: {
-    city: string;
-    temperature: number;
-    date: Date;
-    icon: string;
-    description: string;
-    humidity: number;
-    wind: number;
-  };
-}
-
-const Home: React.FC<HomeProps> = ({ initialData }) => {
-  const [weatherData, setWeatherData] = useState(initialData);
-  const [searchCity, setSearchCity] = useState(""); // For input value
-
-  // Function to handle search and update weather data
-  const handleSearch = async (event: React.FormEvent) => {
-    event.preventDefault();
-    if (searchCity) {
-      const apiUrl = `/api/weather?city=${searchCity}`; //to keep API Key secure
-      const response = await fetch(apiUrl);
-      const data = await response.json();
-      setWeatherData({
-        city: data.weather.name,
-        temperature: data.weather.main.temp,
-        date: new Date(), // Use Date object instead of string
-        icon: data.weather.weather[0].icon,
-        description: data.weather.weather[0].description,
-        humidity: data.weather.main.humidity,
-        wind: data.weather.wind.speed,
-      });
-    }
-  };
-
+export default function Home() {
   return (
     <div className="weather-app">
-      <h1>Weather App</h1>
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          placeholder="Enter city name"
-          value={searchCity}
-          onChange={(e) => setSearchCity(e.target.value)}
-        />
-        <button type="submit">Search</button>
-      </form>
-      <WeatherSearch data={weatherData} />
+      <App />
     </div>
   );
-};
-
-// Server-Side Rendering (SSR) for the initial load
-export const getServerSideProps: GetServerSideProps = async () => {
-  const city = "District X"; // Default city for initial SSR
-  const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/api/weather?city=${city}`;
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-
-  return {
-    props: {
-      initialData: {
-        city: data.weather ? data.weather.name : "District X",
-        temperature: data.weather ? data.weather.main.temp : 20,
-        date: new Date().toISOString(), // Convert to string
-        icon:
-          data.weather && data.weather.weather.length > 0
-            ? data.weather.weather[0].icon
-            : "",
-        description:
-          data.weather && data.weather.weather.length > 0
-            ? data.weather.weather[0].description
-            : "Partly Cloudy",
-        humidity: data.weather ? data.weather.main.humidity : 60,
-        wind: data.weather ? data.weather.wind.speed : 45,
-      },
-    },
-  };
-};
-
-export default Home;
+}
